199113e540f86bcab612ab3d0f21f137b62cf096
梁柱 <liangzhu@77ircloud.com>
2019-05-12 02:25:46 +0800

    feat: 添加路径读取和设置

--- example/example.dart
+++ example/example.dart
@@ -1,4 +1,4 @@
-import 'package:safe/safe.dart';
+import 'package:safe/safe.dart' as safe;
 
 void main() {
   final dynamic v = {
@@ -15,6 +15,56 @@ void main() {
     },
   };
 
-  print(safe(() => v['dog'][0]));
-  print(safe(() => v['dog'][99]));
+  print(safe.ron(() => v['dog'][0]));
+  print(safe.got(v, ['dog', 3, 'sub', 12323, 'fdasfdsa']));
+  print(safe.sot(v, ['dog', 3, 'sub'], '100'));
+
+  // 性能测试: 无任何判断 共耗时 8.312ms
+  var p1 = TimeLog.point();
+  for (var i = 0; i < 100000; i++) {
+    var a = v['dog'][3]['sub'];
+  }
+  TimeLog.long(p1);
+
+  // 性能测试: try-catch, 无错误时, 共耗时 10.33ms
+  var p2 = TimeLog.point();
+  for (var i = 0; i < 100000; i++) {
+    safe.ron(() => v['dog'][3]['sub']);
+  }
+  TimeLog.long(p2);
+
+  // 性能测试: try-catch, 捕获错误时, 共耗时 563.099ms
+  var p3 = TimeLog.point();
+  for (var i = 0; i < 100000; i++) {
+    safe.ron(() => v['dog'][3]['sub']['213213']);
+  }
+  TimeLog.long(p3);
+
+  // 性能测试: 判断路径安全 共耗时 15.835ms
+  var p4 = TimeLog.point();
+  for (var i = 0; i < 100000; i++) {
+    safe.got(v, ['dog', 3]);
+  }
+  TimeLog.long(p4);
+
+  // 性能测试: 判断路径安全 共耗时 13.73ms
+  var p5 = TimeLog.point();
+  for (var i = 0; i < 100000; i++) {
+    safe.sot(v, ['dog', 2], 'new-dog');
+  }
+  TimeLog.long(p5);
+}
+
+class TimeLog {
+  static bool isDev = true;
+
+  static int point() {
+    return DateTime.now().microsecondsSinceEpoch;
+  }
+
+  static void long(num t, [String name = 'Use time: ']) {
+    if (isDev) {
+      print('$name${(DateTime.now().microsecondsSinceEpoch - t) / 1000}ms');
+    }
+  }
 }
--- lib/safe.dart
+++ lib/safe.dart
@@ -1,6 +1,9 @@
 library safe;
 
-dynamic safe(Function fn, [Function errFn]) {
+// Use try catch run fn and return value
+//
+// This is ok: var a = safeRun(()=> v['dog', 2]);
+dynamic ron(Function fn, [Function errFn]) {
   try {
     return fn();
   } catch (err) {
@@ -9,4 +12,63 @@ dynamic safe(Function fn, [Function errFn]) {
     }
     return null;
   }
+}
+
+// Safe set List or Map value
+//
+// This is ok: safeSet(v, ['dog', 10, 'cat', 30], 'newValue');
+dynamic sot(dynamic data, List params, dynamic value) {
+  dynamic last = data;
+  int l = params.length - 1;
+  for (var i = 0; i < l; i++) {
+    var k = params[i];
+    if (last is List) {
+      if (k >= last.length - 1) {
+        last = null;
+        break;
+      }
+      last = last[k];
+    } else if (last is Map) {
+      if (last.containsKey(k) == false) {
+        last = null;
+        break;
+      }
+      last = last[k];
+    } else {
+      last = null;
+      break;
+    }
+  }
+
+  if (last is List || last is Map) {
+    last[params[l]] = value;
+  }
+
+  return data;
+}
+
+// safe get List and Map
+// This is ok: safeGet(v, ['dog', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, '...']);
+dynamic got(dynamic data, List params) {
+  dynamic last = data;
+  for (var i = 0, l = params.length; i < l; i++) {
+    var k = params[i];
+    if (last is List) {
+      if (k >= last.length - 1) {
+        last = null;
+        break;
+      }
+      last = last[k];
+    } else if (last is Map) {
+      if (last.keys.any((v) => v == k) == false) {
+        last = null;
+        break;
+      }
+      last = last[k];
+    } else {
+      last = null;
+      break;
+    }
+  }
+  return last;
 }
